4ad9828bac6b60b87cb1c7901fa2b48fb9ac72a7
isabellacasta198 <isabella.castano03@usc.edu.co>
2025-11-28 11:03:39 -0500

    Create configuracion.php

---
+++ Library/Config/configuracion.php
@@ -0,0 +1,7 @@
+<?php
+// Datos de conexión a MySQL usando XAMPP
+
+$servidor = "localhost";   // Host
+$usuario = "root";         // Usuario por defecto en XAMPP
+$clave = "";               // Contraseña vacía por defecto
+$puerto = 3306;            // Puerto MySQL por defecto
